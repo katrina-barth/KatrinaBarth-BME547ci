8201ab9ff8855722e9dcbd093c471abd22def2cd
Katrina Barth <katrina.barth@duke.edu>
2019-02-05 22:17:08 -0500

    Add Detection of Mis-entries
    Added ability of trachycardia module to handle inputs with uppercase letters and various anticipated mispellings.

--- tachycardia.py
+++ tachycardia.py
@@ -2,14 +2,12 @@
 #Katrina Barth, 02/05/2019
 
 def is_tachycardia(text):
+    text = text.casefold() #Makes the input lowercase, so detection is case insensitive
     if text.find('tachycardia') != -1:
+        return True
+    elif text.find('trachycardia') != -1: #hypothesized mis-spelling
+        return True
+    elif text.find('tac') != -1 and text.find('cardia') !=1: #accounting for several mis-spellings
         return True
     else:
         return False
-
-def main():
-    text = input("Med Entry: ")
-    print(is_tachycardia(text))
-
-if __name__ == "__main__":
-    main()
